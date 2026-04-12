ed86498504fc46c23e1d8be34f60bcd4c20fca1c
Rakibul Islam <rakibulislammondal21@gmail.com>
2026-04-13 04:40:51 +0530

    added resend mail service

--- server/services/mail.service.ts
+++ server/services/mail.service.ts
@@ -1,18 +1,39 @@
 import { transporter } from "../config/mail.config.js";
 import { otpTemplate } from "../templates/otp.template";
 import { otpTemplate_2 } from "../templates/otp.template-2.js";
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+
+// export const sendmail = async (code: string, email: string) => {
+//     try {
+//         await transporter.sendMail({
+//             from: `"ByteBurst Team" <${process.env.MAIL_USER}>`,
+//             to: email,
+//             subject: "Verify Email",
+//             text: code,
+//             html: otpTemplate_2(code)
+//         })
+//     } catch (error: any) {
+//         console.error("Error while sending mail:", error);
+//         throw error
+//     }
+// }
+
+// 
 
 export const sendmail = async (code: string, email: string) => {
     try {
-        await transporter.sendMail({
-            from: `"ByteBurst Team" <${process.env.MAIL_USER}>`,
+        await resend.emails.send({
+            from: 'ByteBurst <onboarding@resend.dev>',
             to: email,
-            subject: "Verify Email",
-            text: code,
-            html: otpTemplate_2(code)
-        })
+            subject: 'OTP Verification',
+            html: otpTemplate_2(code),
+        });
     } catch (error: any) {
         console.error("Error while sending mail:", error);
         throw error
     }
 }
+
